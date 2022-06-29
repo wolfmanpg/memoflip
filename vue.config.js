87d645956225218af63157c231ff5d34fdf51107
wolfmanpg <wolfmanp91@gmail.com>
2022-06-29 18:01:05 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  //publicPath: '/memoflip/',
+  publicPath: process.env.NODE_ENV === "production" ? "/memoflip/" : "/",
 })
